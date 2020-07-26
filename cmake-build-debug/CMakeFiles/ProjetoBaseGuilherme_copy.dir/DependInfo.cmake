ce4271b6ebfb77e15b11e075073e5470de6c5ab4
Guilherme Morais <guitaikion@gmail.com>
2020-07-26 15:33:22 -0300

    Adjust cmake files

---
+++ cmake-build-debug/CMakeFiles/ProjetoBaseGuilherme_copy.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/guilhermemorais/Documents/UFPR/EAD/Compiladores/ProjetoBaseGuilherme/compiladorF.c" "/Users/guilhermemorais/Documents/UFPR/EAD/Compiladores/ProjetoBaseGuilherme/cmake-build-debug/CMakeFiles/ProjetoBaseGuilherme_copy.dir/compiladorF.c.o"
+  "/Users/guilhermemorais/Documents/UFPR/EAD/Compiladores/ProjetoBaseGuilherme/symbolTable/symbolTable.c" "/Users/guilhermemorais/Documents/UFPR/EAD/Compiladores/ProjetoBaseGuilherme/cmake-build-debug/CMakeFiles/ProjetoBaseGuilherme_copy.dir/symbolTable/symbolTable.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../symbolTable"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProjetoBaseGuilherme_copy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoBaseGuilherme_copy.dir/symbolTable/symbolTable.c.o"
+  "CMakeFiles/ProjetoBaseGuilherme_copy.dir/compiladorF.c.o"
+  "ProjetoBaseGuilherme_copy.pdb"
+  "ProjetoBaseGuilherme_copy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoBaseGuilherme_copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
